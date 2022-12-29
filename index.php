fce0a4554e1e949065b46816928ba0322a8dc751
rangapin <rangapin@gmx.com>
2022-12-28 18:57:43 -0600

    connected to db

--- index.php
+++ index.php
@@ -2,3 +2,20 @@
 
 require 'functions.php';
 require 'router.php';
+
+require 'functions.php';
+
+// Connect to the MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+// This should be wrapped in a try-catch.
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
